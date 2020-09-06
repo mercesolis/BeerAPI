ec8aeb4c1e7256d90ada703709c6f9d7018db46c
mercesolis <mersolis@live.com>
2020-09-06 14:47:36 -0500

    data table works

--- Beers/src/app/beer/beer.component.ts
+++ Beers/src/app/beer/beer.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table';
+import { IBeer } from '../i-beer';
+import { MatSort } from '@angular/material/sort';
+import { MatPaginator } from '@angular/material/paginator';
+import { DataService } from '../services/data.service';
+
 
 @Component({
   selector: 'app-beer',
@@ -13,9 +19,18 @@ export class BeerComponent implements OnInit {
     'image_url',
     'abv'
   ];
-  constructor() { }
 
-  ngOnInit(): void {
+  dataSource: MatTableDataSource<IBeer>;
+  @ViewChild(MatSort, {static: true})sort: MatSort;
+  @ViewChild(MatPaginator, {static: true})paginator: MatPaginator;
+  constructor(private dataService: DataService) { }
+
+  async ngOnInit(): Promise<void> {
+    const beers = await this.dataService.getBeers();
+
+    this.dataSource = new MatTableDataSource(beers);
+    this.dataSource.sort = this.sort;
+    this.dataSource.paginator = this.paginator;
   }
 
 }
